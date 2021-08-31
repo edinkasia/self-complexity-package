3a3475da028adc2ccfa1a88f21432866230eb7b7
Jamie <jamie@jumpingrivers.com>
2021-08-31 09:19:46 +0100

    swictnth to ensym

--- R/calculate_h.R
+++ R/calculate_h.R
@@ -40,12 +40,12 @@ calculate_H <- #nolint
 
   # creates a symbol from the string input
   # (needed to use this column name in further operations)
-  id_col <- rlang::ensyms(id_column)
+  id_col <- rlang::ensym(id_column)
   att_quo <- rlang::ensym(att_column)
 
   # sanity checks
   check_input_data(data)
-  check_columns_exist(data, !!!id_col, !!att_quo)
+  check_columns_exist(data, !!id_col, !!att_quo)
 
   # extract the string label for att_column
   att_column <- rlang::as_label(att_quo)
@@ -53,7 +53,7 @@ calculate_H <- #nolint
     purrr::map(~stringr::str_detect(data[[att_column]], .x)) %>%
     purrr::set_names(nm = vector) %>%
     dplyr::bind_cols(data, .) %>%
-    dplyr::group_by(!!!id_col) %>%
+    dplyr::group_by(!!id_col) %>%
     dplyr::mutate(power = 2^ (0:(dplyr::n() - 1))) %>%
     dplyr::mutate(
       dplyr::across(
@@ -68,7 +68,7 @@ calculate_H <- #nolint
       )
     ) %>%
     tidyr::pivot_longer(cols = tidyselect::all_of(vector)) %>%
-    dplyr::group_by(!!!id_col) %>%
+    dplyr::group_by(!!id_col) %>%
     dplyr::count(.data$value) %>%
     dplyr::summarise(
       H_index = log2(length(vector)) -
